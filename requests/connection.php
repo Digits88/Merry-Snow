c5562938d1d216607276d059cfeb61e13276436a
Mamboleoo <contact@mamboleoo.be>
2014-12-02 22:34:49 +0100

    Removing fonts from the page

--- requests/connection.php
+++ requests/connection.php
@@ -4,10 +4,6 @@
 	$user = 'root';
 	$password = 'mysql';
 
-	// $host = 'mysql:host=localhost;dbname=merrySnow';
-	// $user = 'root';
-	// $password = 'mysql';
-	
 	try {
 		$connexion =  new PDO($host, $user, $password);
 		$connexion->query('SET NAMES utf8');
